a546285ae1e7baedc3a314d080fe2a6f521661f3
Xevion <xevion@xevion.dev>
2024-11-26 14:20:18 -0600

    project init

---
+++ factorio-achievements-fixer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace factorio_achievements_fixer;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
