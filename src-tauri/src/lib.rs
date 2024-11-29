48685fad025f205d6caeb05639fb776e1ec2c1a6
Xevion <xevion@xevion.dev>
2024-11-28 19:54:03 -0600

    Separate app main runner, disabled app close request prevention

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,7 +3,7 @@ mod format;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let app = tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
@@ -11,6 +11,23 @@ pub fn run() {
             dirs::find_save_files,
             dirs::choose_best_save_directory,
         ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        // .on_window_event(|_window, event| {
+        //     use tauri::WindowEvent;
+        //     match event {
+        //         WindowEvent::CloseRequested { api, .. } => {
+        //             api.prevent_close();
+        //         }
+        //         _ => {}
+        //     }
+        // })
+        .build(tauri::generate_context!())
+        .expect("error while building tauri application");
+
+    app.run(|_app_handle, event| match event {
+        // tauri::RunEvent::ExitRequested { api, .. } => {
+        //     println!("Exit requested");
+        //     api.prevent_exit();
+        // }
+        _ => {}
+    });
 }
